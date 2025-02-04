b2b4a567ee050efadb9238320b36735b73a87407
wwxuhao <130474990+wwxuhao@users.noreply.github.com>
2025-02-04 09:28:54 -0500

    logo

--- javascripts/discourse/components/custom-footer.js
+++ javascripts/discourse/components/custom-footer.js
@@ -3,4 +3,5 @@ import Component from "@glimmer/component";
 export default class extends Component {
   mainHeading = settings.heading;
   blurb = settings.blurb;
+  site_logo = settings.site_logo
 }
